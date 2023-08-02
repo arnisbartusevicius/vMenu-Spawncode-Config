2bb35c7c23b593a06472ca88d0306f11148e277d
Arnis Bartusevicius <arnisbartusevicius@proton.me>
2023-08-02 14:15:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ for root, dirs, files in os.walk(folder_path): # For each folder and file inside
 
 new_file_names = sorted(set([x for x in new_file_names if not x.endswith("_hi")])) # Remove duplicate files from the set
 print(",\n".join(['"' + name + '"' for name in new_file_names])) # Print out new list of file names in the specified format with a new line after each comma
+
+while True: # Repeats the code
+    quit = input("Would you like to exit? [Y/N]: ").upper() # Asks the user to type y or n and converts the input to uppercase
+    if quit == "Y": # If the user typed "Y":
+        quit() # Quits the program
